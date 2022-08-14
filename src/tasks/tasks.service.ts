51d501c17534f6d39394a357ef3368a6c748a860
Juan Wagner <juanswagner@gmail.com>
2022-08-14 19:32:50 -0300

    implement config vars for keepServiceAlive

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,14 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { EnvConfig } from 'src/config/configuration';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  @Cron(CronExpression.EVERY_MINUTE, { name: 'keepServiceAlive' })
+  constructor(private readonly config: ConfigService<EnvConfig>) {}
+
+  @Cron(CronExpression.EVERY_10_MINUTES, { name: 'keepServiceAlive' })
   async keepServiceAlive() {
+    const { url } = this.config.get('hosting', { infer: true });
+
     try {
-      await fetch('MOCKED_URL');
+      await fetch(url);
+      this.logger.verbose('keep-alive: ok');
     } catch (e) {
       this.logger.warn(
         'There was an error keeping alive keroku',
